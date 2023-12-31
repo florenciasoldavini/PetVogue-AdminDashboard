82f3252a3c58cdcedb664f16e08b8da2c5d8ecd2
Florencia Soldavini <florenciasoldavini@MacBook-Air-de-Florencia.local>
2023-12-31 18:26:28 -0300

    creacion de rutas

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
 import "./App.css";
 import styled from "styled-components";
-import Sidebar from "./components/Sidebar";
-import MainContent from "./components/Dashboard/MainContent";
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Dashboard from "../src/pages/Dashboard"
+import Pets from "../src/pages/Pets"
+import Users from "../src/pages/Users"
+import Appointments from "../src/pages/Appointments"
+import Orders from "../src/pages/Orders"
+import Services from "../src/pages/Services"
+import Sidebar from "../src/components/Sidebar";
+
 function App() {
   return (
-    <Container>
-      <Sidebar />
-      <MainContent />
-    </Container>
+<BrowserRouter>
+<Sidebar/>
+<Routes>
+  <Route path="admin/dashboard" element={Dashboard}/>
+  <Route path="admin/pets" element={Pets}/>
+  <Route path="admin/users" element={Users}/>
+  <Route path="admin/services" element={Services}/>
+  <Route path="admin/orders" element={Orders}/>
+  <Route path="admin/appointments" element={Appointments}/>
+</Routes>
+</BrowserRouter>
+    // <Container>
+    //   <Sidebar />
+    //   <MainContent />
+    // </Container>
   );
 }
 
